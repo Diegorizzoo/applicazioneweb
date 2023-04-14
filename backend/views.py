1128c19e3dc177a202cd417184de8752009424be
Diegorizzoo <rizzodiego2004@gmail.com>
2023-04-14 16:44:24 +0200

    si

--- backend/views.py
+++ backend/views.py
@@ -20,24 +20,25 @@ def backend(request):
 
 
 def quiz(request):
-    unanswered_questions = QA.objects.filter(answered_correctly=False)
+    unanswered_questions = QA.objects.filter(answered_correctly=0)
     if not unanswered_questions:
         # Se non ci sono domande senza answer corretta, mostra il messaggio di completamento
-        return render(request, 'quiz_completed.html')
+        QA.objects.all().update(answered_correctly=0)
+        return render(request, 'quiz_complete.html')
 
     # Seleziona una domanda casuale tra quelle senza answer corretta
     question = random.choice(unanswered_questions)
 
+
     if request.method == 'POST':
-        # Controlla la answer data dall'utente
-        answer = request.POST.get('answer')
-        if answer and answer.strip().upper() == question.answer.upper():
-            # Se la answer è corretta, aggiorna il campo answered_correctly a True
-            question.answered_correctly = True
+        # Check the answer given by the user
+        selected_answer = request.POST.get('answer')
+        if(selected_answer==question.answer):
+            question.answered_correctly = 1
             question.save()
             return redirect('quiz')
         else:
-            # Se la answer è errata, mostra un messaggio di errore e ricarica la pagina
+            # Show an error message if the answer is incorrect
             error = True
     else:
         error = False
