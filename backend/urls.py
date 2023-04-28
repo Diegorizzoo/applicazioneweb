e9f5069de6d099af7239c45ebd042b8bad63e6ad
Diegorizzoo <rizzodiego2004@gmail.com>
2023-04-28 13:14:32 +0200

    parte 3
    ai

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('backend', views.backend, name='backend'),
     path('quiz', views.quiz, name='quiz'),
-    path('quiz_complete', views.quiz_complete, name='quiz_complete')
+    path('quiz_complete', views.quiz_complete, name='quiz_complete'),
+    path('register', views.register_view, name='register'),
+    path('login',views.login_view,name='login'),
 ]
