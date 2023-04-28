ddc1fdfffe9fc2386d66f0a57bee7993476aac32
Diegorizzoo <rizzodiego2004@gmail.com>
2023-04-28 12:58:31 +0200

    si

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class QA(models.Model):
     question = models.CharField(max_length=200)
@@ -8,3 +10,7 @@ class QA(models.Model):
     def __str__(self):
         return self.question
 
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(QA, on_delete=models.CASCADE)
+    answered_correctly = models.BooleanField(default=False)
--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponseRedirect
 from .forms import QAForm
-from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
-from .models import QA
+from .models import QA, UserAnswer
 import random
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
+
+@login_required
 def backend(request):
     if request.method == 'POST':
         form = QAForm(request.POST)
@@ -18,36 +23,83 @@ def backend(request):
         form = QAForm()
     return render(request, 'backend.html', {'form': form})
 
-
+@login_required
 def quiz(request):
-    unanswered_questions = QA.objects.filter(answered_correctly=0)
+    error = False
+
+    unanswered_questions = QA.objects.exclude(useranswer__user=request.user, useranswer__answered_correctly=True)
+
     if not unanswered_questions:
-        # Se non ci sono domande senza answer corretta, mostra il messaggio di completamento
-        QA.objects.all().update(answered_correctly=0)
+        UserAnswer.objects.filter(user=request.user).update(answered_correctly=False)
         return render(request, 'quiz_complete.html')
 
-    # Seleziona una domanda casuale tra quelle senza answer corretta
-    question = random.choice(unanswered_questions)
-
-
     if request.method == 'POST':
-        # Check the answer given by the user
-        selected_answer = request.POST.get('answer')
-        if(selected_answer==question.answer):
-            question.answered_correctly = 1
-            question.save()
-            return redirect('quiz')
-        else:
-            # Show an error message if the answer is incorrect
-            error = True
-    else:
-        error = False
+        answer = request.POST.get('answer')
+        if answer:
+            question = QA.objects.get(pk=request.POST.get('question_id'))
+            if answer.strip().upper() == question.answer.upper():
+                user_answer = UserAnswer.objects.get_or_create(user=request.user, question=question)[0]
+                user_answer.answered_correctly = True
+                user_answer.save()
+
+                if QA.objects.exclude(useranswer__user=request.user, useranswer__answered_correctly=True).count() == 0:
+                    UserAnswer.objects.filter(user=request.user).answered_correctly=0
+
+                if UserAnswer.objects.filter(user=request.user, answered_correctly=False).exists():
+                    return redirect('quiz')
+                else:
+                    UserAnswer.objects.filter(user=request.user).update(answered_correctly=False)
+                    return render(request, 'quiz_complete.html')
+            else:
+                error = True
+
+    question = random.choice(unanswered_questions)
 
     context = {'question': question, 'error': error}
     return render(request, 'quiz.html', context)
 
 
+
+
+
 def quiz_complete(request):
     return render(request, 'quiz_complete.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('backend')
+            else:
+                form.add_error(None, 'Username o password errati')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('backend')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+
 
 
