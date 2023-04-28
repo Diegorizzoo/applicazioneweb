42be92ef470fc8090f4958d89f12c69a5ee7fb5d
Diegorizzoo <rizzodiego2004@gmail.com>
2023-04-28 17:15:54 +0200

    parte 4
    si

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import include
+
 
 urlpatterns = [
     path('backend', views.backend, name='backend'),
@@ -7,4 +10,8 @@ urlpatterns = [
     path('quiz_complete', views.quiz_complete, name='quiz_complete'),
     path('register', views.register_view, name='register'),
     path('login',views.login_view,name='login'),
+    path('api/addQuestion/', views.add_question, name='add_question'),
+    path('api/answer/<int:id>/', views.answer_question, name='answer_question'),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -7,6 +7,15 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import QASerializer
+import requests
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 
 
 @login_required
@@ -14,44 +23,61 @@ def backend(request):
     if request.method == 'POST':
         form = QAForm(request.POST)
         if form.is_valid():
-            question = form.cleaned_data['question']
-            answer = form.cleaned_data['answer']
-            qa = QA(question=question, answer=answer)
-            qa.save()
-            return HttpResponseRedirect('backend')
+            question = form.cleaned_data.get('question')
+            answer = form.cleaned_data.get('answer')
+
+            token, created = Token.objects.get_or_create(user=request.user)
+            response = requests.post('http://localhost:8000/api/addquestion/', {'question': question, 'answer': answer}, headers={'Authorization': f'Token {token.key}'})
+
+            if response.status_code == 201:
+                qa = QA.objects.create(question=question, answer=answer)
+                return redirect('backend')
+            else:
+                form.add_error(None, 'Errore durante la creazione della domanda.')
     else:
         form = QAForm()
-    return render(request, 'backend.html', {'form': form})
+
+    questions = QA.objects.all()
+
+    context = {'form': form, 'questions': questions}
+    return render(request, 'backend.html', context)
+
 
 @login_required
 def quiz(request):
-    error = False
-
     unanswered_questions = QA.objects.exclude(useranswer__user=request.user, useranswer__answered_correctly=True)
 
     if not unanswered_questions:
-        UserAnswer.objects.filter(user=request.user).update(answered_correctly=False)
-        return render(request, 'quiz_complete.html')
+        return render(request, 'quiz_completed.html')
+
+    error = False
 
     if request.method == 'POST':
         answer = request.POST.get('answer')
         if answer:
             question = QA.objects.get(pk=request.POST.get('question_id'))
             if answer.strip().upper() == question.answer.upper():
-                user_answer = UserAnswer.objects.get_or_create(user=request.user, question=question)[0]
-                user_answer.answered_correctly = True
-                user_answer.save()
+                token, created = Token.objects.get_or_create(user=request.user)
+                response = requests.post('http://localhost:8000/api/answer/', {'question_id': question.pk, 'answer': answer}, headers={'Authorization': f'Token {token.key}'})
+                if response.status_code == 200 and response.json()['correct']:
+                    user_answer = UserAnswer.objects.get_or_create(user=request.user, question=question)[0]
+                    user_answer.answered_correctly = True
+                    user_answer.save()
 
-                if QA.objects.exclude(useranswer__user=request.user, useranswer__answered_correctly=True).count() == 0:
-                    UserAnswer.objects.filter(user=request.user).answered_correctly=0
+                    if QA.objects.exclude(useranswer__user=request.user, useranswer__answered_correctly=True).count() == 0:
+                        UserAnswer.objects.filter(user=request.user).delete()
 
-                if UserAnswer.objects.filter(user=request.user, answered_correctly=False).exists():
-                    return redirect('quiz')
+                    if UserAnswer.objects.filter(user=request.user, answered_correctly=False).exists():
+                        return redirect('quiz')
+                    else:
+                        UserAnswer.objects.filter(user=request.user).update(answered_correctly=False)
+                        return render(request, 'quiz_complete.html')
                 else:
-                    UserAnswer.objects.filter(user=request.user).update(answered_correctly=False)
-                    return render(request, 'quiz_complete.html')
+                    error = True
             else:
                 error = True
+        else:
+            error = False
 
     question = random.choice(unanswered_questions)
 
@@ -98,6 +124,48 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_question(request):
+    question = request.data.get('question')
+    answer = request.data.get('answer')
+
+    if not question or not answer:
+        return Response({'error': 'question and answer fields are required'}, status=400)
+
+    qa = QA.objects.create(question=question, answer=answer)
+
+    return Response({'question_id': qa.pk}, status=201)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def answer_question(request):
+    question_id = request.data.get('question_id')
+    answer = request.data.get('answer')
+    if not question_id or not answer:
+        return Response({'error': 'question_id and answer fields are required'}, status=400)
+
+    try:
+        question = QA.objects.get(id=question_id)
+    except QA.DoesNotExist:
+        return Response(status=404)
+
+    if answer.strip().upper() == question.answer.upper():
+        user_answer = UserAnswer.objects.get_or_create(user=request.user, question=question)[0]
+        user_answer.answered_correctly = True
+        user_answer.save()
+        return Response({'correct': True})
+    else:
+        user_answer = UserAnswer.objects.get_or_create(user=request.user, question=question)[0]
+        user_answer.answered_correctly = False
+        user_answer.save()
+        return Response({'correct': False})
+
 
 
 
